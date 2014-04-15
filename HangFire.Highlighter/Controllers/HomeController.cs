8fd58ddc7cb06efb0801a204ecb26da549fe6f6c
Sergey Odinokov <odinserj@gmail.com>
2014-04-15 19:20:23 +0400

    Added highlighting feature

--- HangFire.Highlighter/Controllers/HomeController.cs
+++ HangFire.Highlighter/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using HangFire.Highlighter.Models;
 
@@ -28,16 +31,27 @@ namespace HangFire.Highlighter.Controllers
         [HttpPost]
         public ActionResult Create([Bind(Include = "SourceCode")] CodeSnippet snippet)
         {
-            if (ModelState.IsValid)
+            try
             {
-                snippet.CreatedAt = DateTime.UtcNow;
+                if (ModelState.IsValid)
+                {
+                    snippet.CreatedAt = DateTime.UtcNow;
 
-                // We'll add the highlighting a bit later.
+                    using (StackExchange.Profiling.MiniProfiler.StepStatic("Service call"))
+                    {
+                        snippet.HighlightedCode = HighlightSource(snippet.SourceCode);
+                        snippet.HighlightedAt = DateTime.UtcNow;
+                    }
 
-                _db.CodeSnippets.Add(snippet);
-                _db.SaveChanges();
+                    _db.CodeSnippets.Add(snippet);
+                    _db.SaveChanges();
 
-                return RedirectToAction("Details", new { id = snippet.Id });
+                    return RedirectToAction("Details", new { id = snippet.Id });
+                }
+            }
+            catch (HttpRequestException)
+            {
+                ModelState.AddModelError("", "Highlighting service returned error. Try again later.");
             }
 
             return View(snippet);
@@ -50,6 +64,37 @@ namespace HangFire.Highlighter.Controllers
                 _db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private static async Task<string> HighlightSourceAsync(string source)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    @"http://hilite.me/api",
+                    new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "lexer", "c#" },
+                    { "style", "vs" },
+                    { "code", source }
+                }));
+
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private static string HighlightSource(string source)
+        {
+            // Microsoft.Net.Http does not provide synchronous API,
+            // so we are using wrapper to perform a sync call.
+            return RunSync(() => HighlightSourceAsync(source));
+        }
+
+        private static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return Task.Run<Task<TResult>>(func).Unwrap().GetAwaiter().GetResult();
         }
     }
 }
