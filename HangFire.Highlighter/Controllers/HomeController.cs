b626e876d7a83fa2009262f93a24b135ae7e98c5
Sergey Odinokov <odinserj@gmail.com>
2014-04-15 19:16:59 +0400

    Added controller actions and views

--- HangFire.Highlighter/Controllers/HomeController.cs
+++ HangFire.Highlighter/Controllers/HomeController.cs
@@ -1,18 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using HangFire.Highlighter.Models;
 
 namespace HangFire.Highlighter.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        private readonly HighlighterDbContext _db = new HighlighterDbContext();
+
         public ActionResult Index()
+        {
+            return View(_db.CodeSnippets.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var snippet = _db.CodeSnippets.Find(id);
+            return View(snippet);
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
-	}
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "SourceCode")] CodeSnippet snippet)
+        {
+            if (ModelState.IsValid)
+            {
+                snippet.CreatedAt = DateTime.UtcNow;
+
+                // We'll add the highlighting a bit later.
+
+                _db.CodeSnippets.Add(snippet);
+                _db.SaveChanges();
+
+                return RedirectToAction("Details", new { id = snippet.Id });
+            }
+
+            return View(snippet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
