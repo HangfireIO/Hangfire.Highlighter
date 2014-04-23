4497da3339f373cebae836c01859a223ca8e3cbb
Sergey Odinokov <odinserj@gmail.com>
2014-04-23 13:09:07 +0400

    Installed SignalR

---
+++ HangFire.Highlighter/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using HangFire.Highlighter;
+using Microsoft.Owin;
+using Owin;
+
+[assembly:OwinStartup(typeof(Startup))]
+
+namespace HangFire.Highlighter
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
