bf1b3e8fe731ace354113f1f657d6c845c72f9f6
Sergey Odinokov <odinserj@gmail.com>
2014-05-07 18:38:01 +0400

    SignalR race condition workaround

--- HangFire.Highlighter/Hubs/SnippetHub.cs
+++ HangFire.Highlighter/Hubs/SnippetHub.cs
@@ -1,13 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using HangFire.Highlighter.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace HangFire.Highlighter.Hubs
 {
     public class SnippetHub : Hub
     {
-        public Task Subscribe(int snippetId)
+        public async Task Subscribe(int snippetId)
         {
-            return Groups.Add(Context.ConnectionId, GetGroup(snippetId));
+            await Groups.Add(Context.ConnectionId, GetGroup(snippetId));
+
+            // When a user subscribes a snippet that was already 
+            // highlighted, we need to send it immediately, because
+            // otherwise she will listen for it infinitely.
+            using (var db = new HighlighterDbContext())
+            {
+                var snippet = await db.CodeSnippets
+                    .Where(x => x.Id == snippetId && x.HighlightedCode != null)
+                    .SingleOrDefaultAsync();
+
+                if (snippet != null)
+                {
+                    Clients.Client(Context.ConnectionId)
+                        .highlight(snippet.Id, snippet.HighlightedCode);
+                }
+            }
         }
 
         public static string GetGroup(int snippetId)
