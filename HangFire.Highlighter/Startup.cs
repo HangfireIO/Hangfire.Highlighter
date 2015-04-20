d19e4d16b600cafcc31dd77c9ab624eb1b398e6b
Sergey Odinokov <odinserj@gmail.com>
2015-04-20 18:00:27 +0300

    Update dependencies to latest versions

---
+++ HangFire.Highlighter/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HangFire.Highlighter;
+using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly:OwinStartup(typeof(Startup))]
+
+namespace HangFire.Highlighter
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(
+                "HighlighterDb",
+                new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) });
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
